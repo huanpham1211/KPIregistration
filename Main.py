48966a85a18d6c6cdd23ba35764ab2035924c788
Phạm Nguyên Huân <160609418+huanpham1211@users.noreply.github.com>
2025-04-15 08:47:56 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -267,10 +267,8 @@ def display_registration_form():
                 append_to_sheet(REGISTRATION_SHEET_ID, REGISTRATION_SHEET_RANGE, new_registrations)
                 st.success("Đăng ký thành công!")
                 st.session_state['page'] = "CHỈ TIÊU KPI ĐÃ ĐĂNG KÝ"
-                st.rerun()
             except Exception as e:
-                st.error(f"Lỗi khi ghi dữ liệu vào Google Sheets: {e}")
-
+                st.error(f"Lỗi khi ghi dữ liệu: {e}")
 
 
 
